f9def3532e565f3bcb5fccd628a9933c38887363
ayushcodes1729 <ayushtiwari0803@gmail.com>
2025-07-30 01:46:44 +0530

    Initialize instruction AMM done

---
+++ anchor-amm/programs/anchor-amm/src/state/mod.rs
@@ -0,0 +1,13 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct Config {
+    pub authority: Option<Pubkey>,
+    pub mint_x: Pubkey,
+    pub mint_y: Pubkey,
+    pub fee: u16,
+    pub config_bump: u8,
+    pub lp_bump: u8
+    
+}
