62b6b77aa9978b7df2de2994b4a590a00e40f238
ayushcodes1729 <ayushtiwari0803@gmail.com>
2025-07-30 12:14:51 +0530

    AMM contract done

--- anchor-amm/programs/anchor-amm/src/lib.rs
+++ anchor-amm/programs/anchor-amm/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(deprecated)]
+#![allow(unexpected_cfgs)]
 pub(crate) mod constants;
 pub mod error;
 pub mod instructions;
@@ -14,8 +16,39 @@ declare_id!("6iz8jCLPjHj25FoEbELMyUAQpnGBeqTpZoaAWzg3Qkw3");
 #[program]
 pub mod anchor_amm {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
-    }
+  
+      pub fn initialize(
+          ctx: Context<Initialize>,
+          fee: u16,
+          authority: Option<Pubkey>,
+      ) -> Result<()> {
+          ctx.accounts.initialize(fee, authority, &ctx.bumps)
+      }
+  
+      pub fn deposit(
+          ctx: Context<Deposit>,
+          amount: u64,
+          max_x: u64,
+          max_y: u64
+      ) -> Result<()> {
+          ctx.accounts.deposit(amount, max_x, max_y)
+      }
+  
+      pub fn withdraw(
+          ctx: Context<Withdraw>,
+          amount: u64,
+          min_x: u64,
+          min_y: u64
+      ) -> Result<()> {
+          ctx.accounts.withdraw(amount, min_x, min_y)
+      }
+  
+      pub fn swap(
+          ctx: Context<Swap>,
+          is_x: bool,
+          amount_in: u64,
+          min_amount_out: u64
+      ) -> Result<()> {
+          ctx.accounts.swap(is_x, amount_in, min_amount_out)
+      }
 }
